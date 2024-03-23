aed456f361d9b6319e5f6a314678b2337087a63c
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-03-23 19:26:04 -0300

    implement get user metrics

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -32,6 +32,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .slice((page - 1) * 20, page * 20)
   }
 
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn: CheckIn = {
       id: randomUUID(),
--- src/use-cases/fetch-user-check-ins-history.ts
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -15,9 +15,12 @@ export class FetchUserCheckInsHistoryUseCase {
 
   async execute({
     userId,
-    page
+    page,
   }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
-    const checkIns = await this.checkInsRepository.findManyByUserId(userId, page)
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
 
     return { checkIns }
   }
