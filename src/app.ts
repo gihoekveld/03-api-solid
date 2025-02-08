69e9ecda74c8a423fd4c8a0e1082aa6e6eca7f38
Giselle Hoekveld <gisellehoekveld@gmail.com>
2025-02-08 18:37:12 -0300

    feat: add user role management with ADMIN and MEMBER roles, including token refresh functionality

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import fastifyCookie from '@fastify/cookie'
 import { usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
@@ -10,7 +11,14 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: { expiresIn: '10m' },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
